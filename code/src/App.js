bab150861ce6538f0cf5a3e96c6602a52c993c5a
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2021-05-26 23:20:12 +0700

    EASY BEREZ

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,29 @@
 import MineSiteInput from "./components/MineSiteInput";
-
+import MineSiteOverview from "./components/MineSiteOverview";
 import "./App.scss";
+
+import { //router
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <MineSiteInput/>
+      <Router>
+        <Switch>
+
+          <Route path="/overview" exact>
+            <MineSiteOverview/>
+          </Route>
+
+          <Route path="/" exact>
+            <MineSiteInput/>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- code/src/components/MineSiteInput/index.jsx
+++ code/src/components/MineSiteInput/index.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 import "./styles.scss";
 
 function MineSiteInput() {
   const [names, setNames] = useState([""]);
+  const [oreValues, setOreValues] = useState([""]);
+  const history = useHistory();
+
+  function handleDoneButton(){
+    history.push({
+      pathname: '/overview',
+      state: {oreNames: oreValues}
+    });
+  }
 
   return (
     <div className="mine-site-input">
       Mine Site Input
 
+
       <button id="addMoreButton" onClick={() => setNames(names => [...names, ""])}>Add More</button>
-      <button id="doneButton">Done</button>
+      <button id="doneButton" onClick={handleDoneButton}>Done</button>
 
 
       {names.map((val, index) => (
@@ -19,6 +30,11 @@ function MineSiteInput() {
           <input
             type="text"
             id={`ore${index + 1}`}
+            onChange={(value) => { //tiap ada perubahan dalam input masukkan valuenya
+              let ores = [...oreValues]; //copy isi oreValues saat ini
+              ores[index] = value.target.value; //masukkan value input ke array sementara
+             setOreValues(ores); //masukkan isi array sementara ke oreValues
+            }}
           />
         </div>
       ))}
